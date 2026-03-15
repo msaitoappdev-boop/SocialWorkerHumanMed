d25e858a95de90757cc608c1265fd11f91db0f50
msait <msaitoappdev@gmail.com>
2026-03-15 21:12:01 +0900

    versionCodeを7に変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.msaitodev.socialworker.humanmed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 7
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
