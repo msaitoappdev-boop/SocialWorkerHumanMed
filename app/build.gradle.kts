30f8af55a3fcd42a265a3af4d1704577d44473f9
msait <msaitoappdev@gmail.com>
2026-03-30 16:17:37 +0900

    プレミアムプランの文言見直し

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.msaitodev.socialworker.humanmed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.2.2"
+        versionCode = 14
+        versionName = "1.2.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
