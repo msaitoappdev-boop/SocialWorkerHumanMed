e14df1beb8c7c0449a5a18f707d133f745b87637
msait <msaitoappdev@gmail.com>
2026-04-18 22:02:26 +0900

    通知アイコンの背景色を設定できるように修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.msaitodev.socialworker.humanmed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.3.0"
+        versionCode = 16
+        versionName = "1.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -80,7 +80,7 @@ dependencies {
     // 全てのコアライブラリを Maven 形式で参照
     implementation("com.msaitodev.core:core-common:1.0.0")
     implementation("com.msaitodev.core:core-ads:1.0.2")
-    implementation("com.msaitodev.core:core-notifications:1.0.0")
+    implementation("com.msaitodev.core:core-notifications:1.0.1")
     implementation("com.msaitodev.core:core-navigation:1.0.0")
     implementation("com.msaitodev.core:core-cloud-sync:1.1.1")
     implementation("com.msaitodev.quiz:quiz-core-domain:1.1.1")
